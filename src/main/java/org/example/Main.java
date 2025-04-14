7ed43a0c9f2375e7127340cdcb7bb4f538d455cd
unknown <poluvar51@gmail.com>
2025-04-14 23:34:43 +0300

    Add new class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
         System.out.println("Second commit");
-        }
+        Cat firstCat = new Cat("Борис", "бежевый", 2);
+        Cat secondCat = new Cat("Мурзик", "черный", 3);
+
+        System.out.println("Данные первого котика: "+firstCat.getName()+", "+firstCat.getColor()
+                +", "+firstCat.getAge());
+        System.out.println("Данные второго котика: "+secondCat.getName()+", "+secondCat.getColor()
+                +", "+secondCat.getAge());
+    }
 }
